ba92bcbd94db5fb2ff66d8d63e775c46e79335a1
Camilo Monsalve <cmonsalsilva@gmail.com>
2021-02-13 01:53:22 -0300

    controller_likes, Rt,likes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :tweets do
     resources :likes
   end
-  
-  devise_for :users, controllers: { registrations: 'users/registrations'}
+
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'tweets#index'
