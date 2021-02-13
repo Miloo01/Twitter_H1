33f3e0d1173ba1b5371fca0775963c7b31ab6d5f
Camilo Monsalve <cmonsalsilva@gmail.com>
2021-02-13 01:09:04 -0300

    model Like added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :tweets
+  resources :tweets do
+    resources :likes
+  end
+  
   devise_for :users, controllers: { registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
