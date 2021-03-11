28fcf6c347eb3e3ed79f39d29b0432b51168487d
Camilo Monsalve <cmonsalsilva@gmail.com>
2021-03-10 23:09:38 -0300

    scope tweets_for_me added

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,15 @@ class Tweet < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :linking_users, :through => :likes, :source => :user
   has_many :retweets, class_name: 'Tweet', foreign_key: 'retweet_id'
+  has_many :tweets
+  belongs_to :tweets, optional: true
 
   paginates_per 50
+
+  scope :tweets_for_me, ->(users_list) { where(
+    user_id: users_list.map do |friend|
+        friend.friend_id
+    end
+  ) }
+  
 end
